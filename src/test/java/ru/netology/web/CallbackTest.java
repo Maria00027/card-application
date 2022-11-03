57d18a7a03b243635278a3fb93cfb24bdc00c688
Maria00027 <luma000.27@gmail.com>
2022-11-03 12:48:52 +0300

    fix

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -43,7 +43,7 @@ public class CallbackTest {
         driver.findElement(By.cssSelector("[type = \"tel\"]")).sendKeys("+79992186242");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
     }
 }
